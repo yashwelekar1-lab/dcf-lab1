1d416fe321d41cf271bd9ebe0033bce6f45c01e0
yashwelekar1-lab <yashwelekar1@gmail.com>
2026-08-10 01:19:17 +0530

    Update IntelligenceStoryIntro.tsx

--- src/components/IntelligenceStoryIntro.tsx
+++ src/components/IntelligenceStoryIntro.tsx
@@ -217,7 +217,7 @@ export default function IntelligenceStoryIntro() {
           className="
             absolute
             left-1/2
-            top-[27%]
+            top-[29%]
             z-20
           "
           style={{
@@ -522,7 +522,7 @@ export default function IntelligenceStoryIntro() {
     pointer-events-none
     absolute
     left-1/2
-    top-[88%]
+    top-[80%]
     z-[100]
     -translate-x-1/2
     -translate-y-1/2
