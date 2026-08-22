49c7fdd41ce178f6b633a954258d3edb2bc8fe7f
yashwelekar1-lab <yashwelekar1@gmail.com>
2026-08-23 00:26:01 +0530

    Update IntelligenceStoryIntro.tsx

--- src/components/IntelligenceStoryIntro.tsx
+++ src/components/IntelligenceStoryIntro.tsx
@@ -693,7 +693,7 @@ export default function IntelligenceStoryIntro() {
     pointer-events-none
     absolute
     left-1/2
-    top-[54%]
+    top-1/2
     z-20
   "
   style={{
@@ -777,7 +777,7 @@ export default function IntelligenceStoryIntro() {
     pointer-events-none
     absolute
     left-1/2
-    top-[54%]
+    top-1/2
     z-30
     flex
     flex-col
